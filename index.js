6c2f79fe3113c83e3507afb4909c21e2eb9867c5
Judson Ziehl <108024565+judzdevv@users.noreply.github.com>
2022-06-27 12:19:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ client.once('ready', () => {
     console.log('ZeeBot is online!')
 });
 
-client.login('OTkxMDQyMzE1MjA2NDE4NDc1.Ga_Pzv.tEX_K6hpwPUekRtfyowJUEotlBRWoddg_q41Ic');
+client.login('TOKEN');
